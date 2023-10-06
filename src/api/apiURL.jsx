ce6beee5dc9764704acfd65d47e900e7d8b97990
Mike Xiong <xiong.tchylon@gmail.com>
2023-10-06 10:00:38 +0200

    create function for fetch api's data

---
+++ src/api/apiURL.jsx
@@ -0,0 +1,15 @@
+// Paramètres pour la fonction fetchAsyncGames
+export const gamesURL = 'games';
+export const genresURL = 'genres';
+export const creatorsURL = 'creators';
+export const platformsURL = 'platforms';
+export const developersURL = 'developers';
+
+// Paramètres pour la fonction fetchAsyncGameElements
+export const dlcURL = 'additions';
+export const developmentTeamURL = 'development-team';
+export const gameSeriesURL = 'game-series';
+export const screenshotsURL = 'screenshots';
+export const storesURL = 'stores';
+export const achievementsURL = 'achievements';
+export const moviesURL = 'movies';
