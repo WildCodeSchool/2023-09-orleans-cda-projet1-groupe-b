563c2b1a727e0ded9ca5c2a3f3e4c5be4fd68298
Cardamced <120644423+Cardamced@users.noreply.github.com>
2023-10-09 16:25:23 +0200

    add router skeleton

--- src/router.jsx
+++ src/router.jsx
@@ -1,22 +1,98 @@
+// import * as React from 'react';
+// import './App.css';
+// import Home from './Templates/Home';
+// import Recommandations from './Templates/Recommandations';
+// import Plateforms from './Templates/Plateforms';
+// import Categories from './Templates/Categories';
+// import Action from './Templates/Categories';
+// import Navbar from './components/Navbar';
+// import Error404 from './Templates/Error404';
+// import News from './Templates/News';
+// import PC from './Templates/PlatformGallery';
+// import { Outlet, createBrowserRouter } from 'react-router-dom';
+// import './App.css';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '',
+//     element: (
+//       <>
+//         <Home />
+//         <div className="outlet">
+//           <Outlet />
+//         </div>
+//       </>
+//     ),
+//     children: [
+//       {
+//         path: 'recommandations',
+//         element: (
+//           <>
+//             <Recommandations />
+
+//             <div className="outlet">
+//               <Outlet />
+//             </div>
+//           </>
+//         ),
+//         children: [
+//           {
+//             path: ':recommandations',
+//             element: <News />,
+//           },
+//         ],
+//       },
+//       {
+//         path: 'plateforms',
+//         element: (
+//           <>
+//             <Plateforms />
+
+//             <div className="outlet">
+//               <Outlet />
+//             </div>
+//           </>
+//         ),
+//         children: [
+//           {
+//             path: ':platform',
+//             element: <PC />,
+//           },
+//         ],
+//       },
+//       {
+//         path: 'categories',
+//         element: <Action />,
+//       },
+//       {
+//         path: '*',
+//         element: <Error404 />,
+//       },
+//     ],
+//   },
+// ]);
+
+// export default router;
+
 import * as React from 'react';
 import './App.css';
-import Home from './Pages/Home';
-import Recommandations from './Pages/Recommandations';
-import Plateforms from './Pages/Plateforms';
-import Categories from './Pages/Categories';
-import Error404 from './Pages/Error404';
+import Home from './Templates/Home';
+import Recommandations from './Templates/Recommandations';
+import Plateforms from './Templates/Plateforms';
+import Categories from './Templates/Categories';
+import Action from './Templates/Categories';
 import Navbar from './components/Navbar';
-import News from './Pages/News';
-import Offers from './Pages/Offers';
-import BestSellers from './Pages/BestSellers';
+import Error404 from './Templates/Error404';
+import News from './Templates/News';
+import PC from './Templates/PlatformGallery';
 import { Outlet, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: '',
+    path: '/',
     element: (
       <>
-        <Navbar />
+        <Home />
         <div className="outlet">
           <Outlet />
         </div>
@@ -24,42 +100,32 @@ const router = createBrowserRouter([
     ),
     children: [
       {
-        path: '',
+        index: true,
         element: <Home />,
       },
       {
         path: 'recommandations',
-        element: (
-          <>
-            <Recommandations />
-
-            <div className="outlet">
-              <Outlet />
-            </div>
-          </>
-        ),
+        element: <Recommandations />,
         children: [
           {
-            path: 'news',
+            path: ':recommandations',
             element: <News />,
-          },
-          {
-            path: 'bestsellers',
-            element: <BestSellers />,
-          },
-          {
-            path: 'offers',
-            element: <Offers />,
           },
         ],
       },
       {
         path: 'plateforms',
         element: <Plateforms />,
+        children: [
+          {
+            path: ':platform',
+            element: <PC />,
+          },
+        ],
       },
       {
         path: 'categories',
-        element: <Categories />,
+        element: <Action />,
       },
       {
         path: '*',
