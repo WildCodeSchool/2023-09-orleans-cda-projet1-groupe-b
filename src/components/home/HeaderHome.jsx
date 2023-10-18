6d4342635c4a17f88bd05c90c68a7dceb626a491
Mike Xiong <xiong.tchylon@gmail.com>
2023-10-18 09:39:04 +0200

    remove else

--- src/components/home/HeaderHome.jsx
+++ src/components/home/HeaderHome.jsx
@@ -30,74 +30,74 @@ const imageVariants = {
 export default function HeaderHome({ imageHeader, isLoaded }) {
   if (!isLoaded) {
     return null;
-  } else {
-    return (
-      <>
-        <div className="absolute h-screen w-screen overflow-hidden">
-          <motion.div
-            variants={containerVariants}
-            initial="hidden"
-            animate="visible"
-            custom="0"
-            className="image-mask grid h-full w-full grid-cols-1  md:grid-cols-4"
-          >
-            <div className="absolute z-50 h-full w-full bg-gradient-to-r from-dark to-dark/0"></div>
-            <motion.div
-              className="hidden md:block"
-              variants={imageVariants}
-              custom={-200}
-            >
-              {/* Affichage des images si au dessus de 768px */}
-              <img
-                src="/images/mario.webp"
-                alt=""
-                className="h-full w-full object-cover"
-              />
-            </motion.div>
-            <motion.div
-              className="hidden md:block"
-              variants={imageVariants}
-              custom={200}
-            >
-              <img
-                src="/images/masterchief.webp"
-                alt=""
-                className="h-full w-full object-cover"
-              />
-            </motion.div>
-            <motion.div
-              className="hidden md:block"
-              variants={imageVariants}
-              custom={-200}
-            >
-              <img
-                src="/images/kratos.webp"
-                alt=""
-                className="h-full w-full object-cover"
-              />
-            </motion.div>
-            <motion.div
-              className="hidden md:block"
-              variants={imageVariants}
-              custom={200}
-            >
-              <img
-                src="/images/jinx.webp"
-                alt=""
-                className="h-full w-full object-cover"
-              />
-            </motion.div>
-            {/* Affichage de l'image si en dessous de 768px */}
-            <div className="block md:hidden">
-              <img
-                src={imageHeader.image_background}
-                alt=""
-                className="h-full w-full object-cover"
-              />
-            </div>
-          </motion.div>
-        </div>
-      </>
-    );
   }
+
+  return (
+    <>
+      <div className="absolute h-screen w-screen overflow-hidden">
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          custom="0"
+          className="image-mask grid h-full w-full grid-cols-1  md:grid-cols-4"
+        >
+          <div className="absolute z-50 h-full w-full bg-gradient-to-r from-dark to-dark/0"></div>
+          <motion.div
+            className="hidden md:block"
+            variants={imageVariants}
+            custom={-200}
+          >
+            {/* Affichage des images si au dessus de 768px */}
+            <img
+              src="/images/mario.webp"
+              alt=""
+              className="h-full w-full object-cover"
+            />
+          </motion.div>
+          <motion.div
+            className="hidden md:block"
+            variants={imageVariants}
+            custom={200}
+          >
+            <img
+              src="/images/masterchief.webp"
+              alt=""
+              className="h-full w-full object-cover"
+            />
+          </motion.div>
+          <motion.div
+            className="hidden md:block"
+            variants={imageVariants}
+            custom={-200}
+          >
+            <img
+              src="/images/kratos.webp"
+              alt=""
+              className="h-full w-full object-cover"
+            />
+          </motion.div>
+          <motion.div
+            className="hidden md:block"
+            variants={imageVariants}
+            custom={200}
+          >
+            <img
+              src="/images/jinx.webp"
+              alt=""
+              className="h-full w-full object-cover"
+            />
+          </motion.div>
+          {/* Affichage de l'image si en dessous de 768px */}
+          <div className="block md:hidden">
+            <img
+              src={imageHeader.image_background}
+              alt=""
+              className="h-full w-full object-cover"
+            />
+          </div>
+        </motion.div>
+      </div>
+    </>
+  );
 }
