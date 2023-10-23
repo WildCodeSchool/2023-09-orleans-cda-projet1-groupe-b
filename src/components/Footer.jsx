60f306ecb21c6c565298f28ac0f76af92f10c6c6
herve perel <herveperel@gmail.com>
2023-10-23 16:42:52 +0200

    correction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 export default function Footer() {
   return (
     <>
-      <div className="flex justify-between pt-4">
-        <p className="ms-10 text-[#525252]">About the team</p>
-        <p className="me-10 text-[#525252]">GamePlus ® Legal Notices</p>
-      </div>
+      <footer className="h-14 w-screen bg-dark">
+        <div className="flex justify-between pt-4">
+          <p className="ms-10 text-[#525252]">About the team</p>
+          <p className="me-10 text-[#525252]">GamePlus ® Legal Notices</p>
+        </div>
+      </footer>
     </>
   );
 }
