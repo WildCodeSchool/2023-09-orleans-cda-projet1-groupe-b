c7700d2b968eb0219270a131cee99c51e26cfd12
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-10-26 16:06:43 +0200

    added achievements section

--- src/components/gameshow/Box.jsx
+++ src/components/gameshow/Box.jsx
@@ -1,19 +1,19 @@
-import { useState, useRef, useEffect } from "react";
-import { AnimatePresence, motion, usePresence } from "framer-motion";
-import { gsap } from "gsap";
+import { useRef, useEffect } from 'react';
+import { usePresence } from 'framer-motion';
+import { gsap } from 'gsap';
 
 export default function Box() {
-    const ref = useRef(null);
-    const [isPresent, safeToRemove] = usePresence();
-  
-    useEffect(() => {
-      if (!isPresent) {
-        gsap.to(ref.current, {
-          opacity: 0,
-          onComplete: () => safeToRemove?.()
-        });
-      }
-    }, [isPresent, safeToRemove]);
-  
-    return <div className="box" ref={ref} />;
-  }
+  const ref = useRef(null);
+  const [isPresent, safeToRemove] = usePresence();
+
+  useEffect(() => {
+    if (!isPresent) {
+      gsap.to(ref.current, {
+        opacity: 0,
+        onComplete: () => safeToRemove?.(),
+      });
+    }
+  }, [isPresent, safeToRemove]);
+
+  return <div className="box" ref={ref} />;
+}
