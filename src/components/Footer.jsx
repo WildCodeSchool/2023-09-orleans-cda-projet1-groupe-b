2b905be4bada562b9fe1b95dcc2e892649fd1b1d
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-10-30 15:04:55 +0100

    added margin top to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="h-14 w-screen bg-dark mt-14">
+      <footer className="mt-14 h-14 w-screen bg-dark">
         <div className="flex justify-between pt-4">
           <p className="ms-10 text-[#525252]">About the team</p>
           <p className="me-10 text-[#525252]">GamePulse ® Legal Notices</p>
