457b08f44e91de88bd8662bb8d349fded9134155
Mike Xiong <xiong.tchylon@gmail.com>
2023-10-30 16:33:16 +0100

    create about the team page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
     <>
       <footer className="h-14 w-screen bg-dark">
         <div className="flex justify-between pt-4">
-          <p className="ms-10 text-[#525252]">About the team</p>
+          <Link to="/about-the-team">
+            <p className="ms-10 text-light/70 hover:text-light">
+              About the team
+            </p>
+          </Link>
           <p className="me-10 text-[#525252]">GamePlus ® Legal Notices</p>
         </div>
       </footer>
