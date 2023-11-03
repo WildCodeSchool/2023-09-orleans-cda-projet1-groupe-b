b733a25ef29ffa143c08b70e5102908e2ebc89d9
Mike Xiong <xiong.tchylon@gmail.com>
2023-11-03 14:34:15 +0100

    fixe css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar';
 import ScrollTop from './components/ScrollTop';
 import Footer from './components/Footer';
 import SearchBar from './components/search/SearchBar';
-import './App.css';
 
 export default function App() {
   const location = useLocation();
