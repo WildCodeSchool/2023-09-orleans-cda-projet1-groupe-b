4723c330a16df2f0757806f729170afa516f7ed5
Mike Xiong <xiong.tchylon@gmail.com>
2023-11-03 10:53:25 +0100

    fixe routes

--- src/router.jsx
+++ src/router.jsx
@@ -12,61 +12,64 @@ import AboutTheTeam from './components/AboutTheTeam';
 import Error404 from './components/Error404';
 import Platforms from './components/genres/Platforms';
 
-export default createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: 'recommandations',
-        element: <Recommandations />,
-        children: [
-          {
-            path: 'news',
-            element: <News />,
-          },
-          {
-            path: 'bestsellers',
-            element: <BestSellers />,
-          },
-          {
-            path: 'offers',
-            element: <Offers />,
-          },
-        ],
-      },
-      {
-        path: '/games/:gameId',
-        element: <GameShow />,
-      },
-      {
-        path: '/genres/:slug',
-        element: <Categories />,
-      },
-      {
-        path: '/games',
-        element: <Search />,
-      },
-      {
-        path: '/about-the-team',
-        element: <AboutTheTeam />,
-      },
-      {
-        path: '/404',
-        element: <Error404 />,
-      },
-      {
-        path: '*',
-        element: <Navigate to="/404" />,
-      },
-      {
-        path: '/platforms/:id',
-        element: <Platforms />,
-      },
-    ],
-  },
-]);
+export default createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: 'recommandations',
+          element: <Recommandations />,
+          children: [
+            {
+              path: 'news',
+              element: <News />,
+            },
+            {
+              path: 'bestsellers',
+              element: <BestSellers />,
+            },
+            {
+              path: 'offers',
+              element: <Offers />,
+            },
+          ],
+        },
+        {
+          path: '/games/:gameId',
+          element: <GameShow />,
+        },
+        {
+          path: '/genres/:slug',
+          element: <Categories />,
+        },
+        {
+          path: '/games',
+          element: <Search />,
+        },
+        {
+          path: '/about-the-team',
+          element: <AboutTheTeam />,
+        },
+        {
+          path: '/404',
+          element: <Error404 />,
+        },
+        {
+          path: '*',
+          element: <Navigate to="/404" />,
+        },
+        {
+          path: '/platforms/:id',
+          element: <Platforms />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
